375eced553c3aa48b0dd8a4637dfd79748d0f39a
guzajakub <56091386+guzajakub@users.noreply.github.com>
2020-03-13 16:19:03 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+    public:
+    float radius_;
+} 
+
+int main() {
+    Circle c1, c2;
+    c1.radius_=6;
+    c2.radius_=8;
+return 0;
+}
